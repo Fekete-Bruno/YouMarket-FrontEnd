024afe8cc8dc3d6f7df0140b2bfc3163e6380605
Fekete-Bruno <brunohffekete@gmail.com>
2022-09-16 07:21:38 -0300

    feat: added product details

--- src/Components/ProductPage/ProductPage.js
+++ src/Components/ProductPage/ProductPage.js
@@ -1,13 +1,47 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-
+import styled from "styled-components";
+import { getProductById, handleError } from "../../services/axiosHandler";
+import MainMenu from "../MainMenu/MainMenu";
+import Title from "../Title/Title";
 
 export default function ProductPage(){
     const { productId } = useParams();
-    console.log (productId);
+    const [product,setProduct] = useState("");
+    console.log(product);
+
+    useEffect(()=>{
+        const request = getProductById(productId);
+        request.then((response)=>setProduct(response.data)).catch((error)=>handleError(error));
+    }
+    ,[]);
 
     return(
-        <div>
-            This is the product page!!!
-        </div>
+        <>
+        <Title />
+        <ProductWrapper>
+            <h1>{product.title}</h1>
+            <img src={product.image} alt={product.title} />
+            <h2>Price: $ {product.price}</h2>
+            <h3>Quantity: {product.amount}</h3>
+            <p>{product.description}</p>
+        </ProductWrapper>
+        <MainMenu />
+        </>
     );
 }
+
+const ProductWrapper = styled.div`
+    margin: 8vh 0;
+    min-height: 84vh;
+    background-color: rgb(202, 210, 197);
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    text-align: center;
+    img{
+        height: 50vh;
+        width: 50vh;
+        object-fit: cover;
+    }
+`;
