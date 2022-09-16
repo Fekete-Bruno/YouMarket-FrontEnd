46c48ca243bf5e0193764b1b4769aaa3f5bd637e
Yan Amaral <yan@pop-os.localdomain>
2022-09-16 09:31:54 -0300

    feat: checkout page visuals with mocked data

--- src/Components/App.js
+++ src/Components/App.js
@@ -3,6 +3,7 @@ import { GlobalStyle } from "../styles/globalStyles";
 import MainPage from "./MainPage/MainPage";
 import LoginPage from "./LoginPage/LoginPage";
 import SignUpPage from "./SignUpPage/SignUpPage";
+import CheckoutPage from "./Checkout/CheckoutPage";
 
 export default function App() {
   return (
@@ -12,6 +13,7 @@ export default function App() {
         <Route path="/" element={<MainPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/checkout" element={<CheckoutPage />} />
       </Routes>
     </BrowserRouter>
   );
