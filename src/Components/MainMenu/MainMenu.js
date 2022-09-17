d98da0bcca6897abade4e0d04a2905f5b85bb204
Fekete-Bruno <brunohffekete@gmail.com>
2022-09-16 21:29:33 -0300

    feat: added a sidebar for next features

--- src/Components/MainMenu/MainMenu.js
+++ src/Components/MainMenu/MainMenu.js
@@ -1,16 +1,35 @@
+import { useState } from "react";
 import styled from "styled-components";
 
 export default function MainMenu(){
-    return(
-        <MenuWrapper>
-            <ion-icon name="reorder-three-sharp"></ion-icon>
-            
-            <ion-icon name="person-circle-sharp"></ion-icon>
+    const [selected,setSelected] = useState (false);
 
-            <ion-icon name="cart-sharp"></ion-icon>
-        </MenuWrapper>
+    return(
+        <>
+            <Sidebar selected={selected}>
+            <h1>New features soon!</h1>
+            </Sidebar>
+            <MenuWrapper>
+                <ion-icon name="person-circle-sharp"></ion-icon>
+
+                <ion-icon name="reorder-three-sharp" onClick={()=>setSelected(!selected)}></ion-icon>
+
+                <ion-icon name="cart-sharp"></ion-icon>
+            </MenuWrapper>
+        </>
     );
 }
+const Sidebar = styled.div`
+    position: fixed;
+    left: 0;
+    bottom: 8vh;
+    text-align: center;
+    height: ${(props)=>props.selected?"0":"22vh"};
+    width: 100vw;
+    background-color: rgba(47, 62, 70,0.95);
+    color: rgb(202, 210, 197);
+    transition: all 500ms;
+`;
 
 const MenuWrapper = styled.div`
     display: flex;
